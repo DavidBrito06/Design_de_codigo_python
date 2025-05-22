778bb5cd7aa47b6369cafad9a8cf53061cb68924
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-05-22 09:57:15 -0300

    Implement factories for Calculator1 and Calculator2, updating routes to use factory methods for instantiation

--- src/main/routes/calculators.py
+++ src/main/routes/calculators.py
@@ -1,10 +1,20 @@
 from flask import Blueprint, jsonify,request
-from src.calculators.calculator_1 import Calculator1
+from src.main.factories.calculator1_factory import calculator_1_factory
+from src.main.factories.calculator2_factory import calculator_2_factory
+
 
 calc_rout_bp = Blueprint("calc_routes", __name__)
 
 @calc_rout_bp.route("/calculator/1", methods=["POST"])
 def calculator_1():
-    calc = Calculator1()
-    response = calc.Calculator(request)
+    calc = calculator_1_factory()
+    response = calc.calculate(request)
+
+    return jsonify(response),200
+
+@calc_rout_bp.route("/calculator/2", methods=["POST"])
+def calculator_2():
+    calc = calculator_2_factory()
+    response = calc.calculate(request)
+
     return jsonify(response),200
