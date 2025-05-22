fe9be1aba448f3adce2fd161f97a34b31bb84236
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-05-22 09:38:53 -0300

    Add tests for Calculator2 integration and standard derivation functionality

--- src/calculators/calculator_2_test.py
+++ src/calculators/calculator_2_test.py
@@ -1,10 +1,17 @@
 from .calculator_2 import Calculator2
-from typing import Dict
+from typing import Dict, List
 from src.drivers.numpy_handler import NumpyHandler
+from src.drivers.interfaces.driver_hander_interface import DriverHandlerInterface
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
+
+
+class MockDriverHandler(DriverHandlerInterface):
+    def standard_derivation(self, numbers: List[float]) -> float:
+        # Mock implementation of standard_derivation
+        return 3
 
 
 def test_calculate_integration():
@@ -16,3 +23,15 @@ def test_calculate_integration():
 
     assert isinstance(format_response, dict)
     assert format_response == {'data': {'calculator': 2, 'result': 0.14}}
+
+
+
+def test_calculate():
+    mock_request = MockRequest({"numbers": [1, 2, 3]})
+    driver = MockDriverHandler()
+    calculator_2 = Calculator2(driver)
+    format_response =calculator_2.calculate(mock_request)
+    print(format_response)
+
+    assert isinstance(format_response, dict)
+    assert format_response == {'data': {'calculator': 2, 'result': 0.33}}
