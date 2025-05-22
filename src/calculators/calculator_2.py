9de85e72d39f1104b828857e481f79e02937373c
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-05-22 08:40:22 -0300

    Enhance Calculator2 with data processing and integration of NumpyHandler for standard derivation

--- src/calculators/calculator_2.py
+++ src/calculators/calculator_2.py
@@ -1,12 +1,13 @@
 from flask import request as FlaskRequest
 from typing import Dict, List
+from src.drivers.numpy_handler import NumpyHandler
 
 class Calculator2:
     def calculate(self, request: FlaskRequest) -> Dict:#type: ignore[no-untyped-def]
         body = request.json
-        print(body)
         input_data = self.__validate_body(body)
-        print(input_data)
+        self.__process_data(input_data)
+       
 
     def __validate_body(self, body: Dict) -> List[float]:
         if "numbers" not in body:
@@ -15,4 +16,15 @@ class Calculator2:
 
         input_data = body["numbers"]
         return input_data
+    
+    def __process_data(self, input_data: List[float]) -> float:
+        # Example processing: sum of numbers
+        numpy_handler = NumpyHandler()
+        first_process_result  = [(num *11) ** 0.95 for num in input_data]
+        print(first_process_result)
+        result =numpy_handler.standard_derivation(first_process_result)
+        print(result)
+        return 1/result
+        
+
     
