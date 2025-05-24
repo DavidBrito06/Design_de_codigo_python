eb7ad2c3488ba316c665aede3cab2e20543cb9cd
DavidBrito06 <davidbritodejesus1236@gmail.com>
2025-05-24 13:52:48 -0300

    Refactor tests in Calculator3 and Calculator2 to use mock implementations for variance and standard deviation calculations

--- src/calculators/calculate_3_test.py
+++ src/calculators/calculate_3_test.py
@@ -1,16 +1,26 @@
 from .calculator_3 import Calculator3
 from src.drivers.numpy_handler import NumpyHandler
 from pytest import raises
-from typing import Dict
+from typing import Dict, List
 
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
         self.json = body
 
+class MockDriverHandlerError:
+    def variance(self, numbers: List[float]) -> float:
+        # Mock implementation of variance
+        return 3
+
+class MockDriverHandler:
+    def variance(self, numbers: List[float]) -> float:
+        # Mock implementation of variance
+        return 1000000
+
 def test_calculate_with_variance_erro():
     mock_request = MockRequest({"numbers": [1, 2, 3, 4, 5]})
-    calculator_3 = Calculator3(NumpyHandler())
+    calculator_3 = Calculator3(MockDriverHandlerError())
     with raises(Exception) as exc_info:
          calculator_3.calculate(mock_request)
 
@@ -18,13 +28,13 @@ def test_calculate_with_variance_erro():
 
 def test_calculate():
     mock_request = MockRequest({"numbers": [1, 1, 1, 1, 100]})
-    calculator_3 = Calculator3(NumpyHandler())
+    calculator_3 = Calculator3(MockDriverHandler())
 
 
     response = calculator_3.calculate(mock_request)
     print()
     print(response)
 
-    assert response == {'data': {'calculator': 3, 'result': 1568, 'success': True}}
+    assert response == {'data': {'calculator': 3, 'result': 1000000, 'success': True}}
 
     
